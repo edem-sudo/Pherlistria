c84f377564f9d9595055a1948f264cd05278a27c
edem <ahtakfflsmsdkemf@gmail.com>
2025-12-17 23:14:33 +0900

    add Inventory

--- src/pages/Inventory.jsx
+++ src/pages/Inventory.jsx
@@ -1,9 +1,39 @@
-function Inventory(inventory, setInventory){
+import { useState } from 'react';
 
-  console.log({inventory});
+function Inventory({inventory, setInventory}){
+
+  console.log({inventory}); //[{Pherlisite}, {Pherlisite}, ...]
 
   return(<>
-    
+    <h2>Inventory</h2>
+
+
+    <ul>
+    {
+      inventory.map((stone,i)=>{
+
+        const compositionText = Object.entries(stone.composition).map(
+          ([key, val]) => (
+            <div key={key}>
+              <span className={`${key}`}>{key}</span>:{"  "}{val}%
+            </div>
+          )
+        );
+
+        return(
+          <li className="item" key={stone.id}>
+            <details>
+              <summary><strong><span className={`${stone.type}`}>{stone.type}</span> #{inventory.length - i}</strong></summary>
+              <div>
+                {compositionText}
+              </div>
+            </details>
+          </li>
+        );
+      })
+    }
+    </ul>
+
   </>)
 }
 
--- src/rules/generator/RhesitePherlisite.js
+++ src/rules/generator/RhesitePherlisite.js
@@ -3,3 +3,4 @@ import { STANDARD_COMPOSITION_TABLE } from '../composition/standardComposition.j
 import { ELEMENT_DENSITY } from '../constants/density.js';
 import { UT_TRIMS } from '../constants/trims.js';
 import { random } from '../../utils/random.js';
+import { Common_Pherlisite } from './CommonPherlisite.js';
